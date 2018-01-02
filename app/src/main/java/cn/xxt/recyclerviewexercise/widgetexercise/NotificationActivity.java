fe344e2a55bdf7447edc8275ec84ec3d6ede170e
Martin <1457453696@qq.com>
2018-01-02 21:55:31 +0800

    notification练习

--- app/src/main/java/cn/xxt/recyclerviewexercise/widgetexercise/NotificationActivity.java
+++ app/src/main/java/cn/xxt/recyclerviewexercise/widgetexercise/NotificationActivity.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RemoteViews;
 
 import cn.xxt.recyclerviewexercise.R;
 /**
@@ -67,19 +68,69 @@ public class NotificationActivity extends AppCompatActivity {
     }
 
     /**
-     * 显示带有信息的
+     * 显示带有信息的notification
      * @param view
      */
     public void clickBtnWithInfoNotify(View view) {
+        //在Notification的右下角显示信息
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.face1).setContentTitle("info")
+                .setContentText("文本信息显示在右下角").setContentInfo("信息");
+        notificationManager.notify(4, builder.build());
+        //在Notification的右下角显示数字（信息的特殊形式）
+        Notification.Builder builder1 = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.face2).setContentTitle("Number")
+                .setContentText("数字显示在右下角").setNumber(25);
+        notificationManager.notify(5, builder1.build());
 
     }
 
+    /**
+     * 显示进度条的notification
+     * @param view
+     */
     public void clickBtnWithProgressNotify(View view) {
+        //显示确定的进度条，setProgress()方法的第三个参数必须为false
+        //其第一个参数表示进度最大值，第二个参数表示当前值
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.face3).setContentTitle("determinate progress")
+                .setContentText("显示进度的进度条").setProgress(100, 25, false);
+        notificationManager.notify(6, builder.build());
 
+        //显示不确定进度的进度条 第三个参数必须为true
+        Notification.Builder builder1 = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.face4).setContentTitle("Indeterminate progress")
+                .setContentText("不确定进度的进度条").setProgress(100, 25, true);
+        notificationManager.notify(7, builder1.build());
+
+    }
+
+    /**
+     * 永久存在的notification
+     * @param view
+     */
+    public void clickBtnPermanentNotify(View view) {
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.face5).setContentTitle("短信内容")
+                .setOngoing(true).setContentText("贼他么6");
+        notificationManager.notify(8, builder.build());
+    }
+
+    /**
+     * 自定义的notification setContent方法，关键是remoteview。
+     * @param view
+     */
+    public void clickBtnCustomNotify(View view) {
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.custom_notification_layout);
+        //设置布局中TextView显示的文本
+        remoteViews.setTextViewText(R.id.tv_custom, "更新显示的自定义文本");
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.face5).setContent(remoteViews);
+        notificationManager.notify(9, builder.build());
     }
 
     public void clickBtnClearAllNotify(View view) {
-
+        notificationManager.cancelAll();
     }
 
 }
